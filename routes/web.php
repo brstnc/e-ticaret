eda9780bd1b51abb53b95043a28aafcd270b21cc
Barış Tunç <baristunc_04@hotmail.com>
2019-04-16 12:42:17 +0300

    Sadece giriş yapan üyelerin ulaşabileceği sayfalara yetkilendirme yapıldı

--- routes/web.php
+++ routes/web.php
@@ -12,34 +12,22 @@
 */
 
 Route::get('/', 'HomePageController@index')->name('homepage');
-
 Route::get('/category/{slug_category_name}', 'CategoryController@index')->name('category');
-
 Route::get('/product/{slug_product_name}', 'ProductController@index')->name('product');
-
 Route::post('/search', 'ProductController@search')->name('search');
 Route::get('/search', 'ProductController@search')->name('search');
-
-
 Route::get('/basket', 'BasketController@index')->name('basket');
-
-Route::get('/payment', 'PaymentController@index')->name('payment');
-
-Route::get('/orders', 'OrdersController@index')->name('orders');
-
-Route::get('/orders/{id}', 'OrdersController@detail')->name('order');
-
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/payment', 'PaymentController@index')->name('payment');
+    Route::get('/orders', 'OrdersController@index')->name('orders');
+    Route::get('/orders/{id}', 'OrdersController@detail')->name('order');
+});
 Route::group(['prefix' => 'user'], function() {
-
     Route::get('/signin', 'UserController@signin_form')->name('user.signin');
     Route::post('/signin', 'UserController@signin');
-
     Route::get('/signup', 'UserController@signup_form')->name('user.signup');
     Route::post('/signup', 'UserController@signup');
-
     Route::post('/logout', 'UserController@logout')->name('user.logout');
-
     Route::get('/active/{key}', 'UserController@active')->name('active');
-
 });
 
